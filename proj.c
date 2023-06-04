086db2e90f3516a98db4d20d585bc0a6abdede4f
BegadElsamadesy <135548165+BegadElsamadesy@users.noreply.github.com>
2023-06-04 22:33:01 +0300

    proj.c

--- proj.c
+++ proj.c
@@ -2,34 +2,33 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-unsigned char tick=0;
 char pin[6];
 
 void Timer_1_init(){
-	TCNT1	=	0;									//initialize timer counter to 0
+	TCNT1	=	0;								//initialize timer counter to 0
 	OCR1A	=	978;								//every tick is 1 second
-	TIMSK	|=	(1<<OCIE1A);						//Output Compare A Match Interrupt Enable
-	TCCR1A	|=	(1<<FOC1A); 						//non pmw pin
+	TIMSK	|=	(1<<OCIE1A);							//Output Compare A Match Interrupt Enable
+	TCCR1A	|=	(1<<FOC1A); 							//non pmw pin
 	TCCR1B	|=	(1<<WGM12)	|	(1<<CS12)	|	(1<<CS10);	//compare mood ||  clock(1Mhz)/1024 pre-scaler
 }
 
 void INT0_init(){
-	DDRD	&=	(~(1<<PD2));						//INT 0 PAUSE
-	PORTB 	|= 	(1<<PD2);         					// Activate the internal pull up resistor at PD2
+	DDRD	&=	(~(1<<PD2));							//INT 0 PAUSE
+	PORTB 	|= 	(1<<PD2);         						// Activate the internal pull up resistor at PD2
 	MCUCR	|=	(1<<ISC01);							//INT 1 with falling edge
 	GICR	|=	(1<<INT0);							//activate external interrupt of INT 0
 }
 
 void INT1_init(){
-	DDRD	&=	(~(1<<PD3));						//INT 1 RESET
-	MCUCR	|=	(1<<ISC11)	|	(1<<ISC10);			//INT 1 with rising edge
+	DDRD	&=	(~(1<<PD3));							//INT 1 RESET
+	MCUCR	|=	(1<<ISC11)	|	(1<<ISC10);				//INT 1 with rising edge
 	GICR	|=	(1<<INT1);							//activate external interrupt of INT 1
 }
 
 void INT2_init(){
-	DDRB	&=	(~(1<<PB2));						//INT 2 RESUME
-	PORTB 	|= 	(1<<PB2);        					// Activate the internal pull up resistor at PB2
-	MCUCSR	&=	(~(1<<ISC2));						//INT 2 with falling edge
+	DDRB	&=	(~(1<<PB2));							//INT 2 RESUME
+	PORTB 	|= 	(1<<PB2);        						// Activate the internal pull up resistor at PB2
+	MCUCSR	&=	(~(1<<ISC2));							//INT 2 with falling edge
 	GICR	|=	(1<<INT2);							//activate external interrupt of INT 2
 }
 
@@ -37,25 +36,25 @@ ISR(TIMER1_COMPA_vect){
 	pin[0]++;
 	if(pin[0]==10){									//digit 1 seconds
 		pin[0]=0;
-		pin[1]++;
+		pin[1]++;								//Increament seconds 2nd digit
 		if(pin[1]==6){								//digit 2 seconds
 			pin[1]=0;
-			pin[2]++;
+			pin[2]++;							//Increament mins 1st digit
 		}
 	}
-	if(pin[2]==10){
+	if(pin[2]==10){									//digit 1 mins
 		pin[2]=0;
-		pin[3]++;
-		if(pin[3]==6){
+		pin[3]++;								//Increament mins 2nd digit
+		if(pin[3]==6){								//digit 2 mins
 			pin[3]=0;
-			pin[4]++;
+			pin[4]++;							//Increament hours 1st digit
 		}
 	}
-	if(pin[4]==10){
+	if(pin[4]==10){									//digit 1 hours
 		pin[4]=0;
-		pin[5]++;
-		if(pin[5]==6){
-			pin[5]=0;
+		pin[5]++;								//Increament hours 2nd digit
+		if(pin[5]==6){								//digt 2 hours
+			pin[5]=0;							//Reset all 
 			pin[4]=0;
 			pin[3]=0;
 			pin[2]=0;
@@ -66,7 +65,7 @@ ISR(TIMER1_COMPA_vect){
 }
 
 ISR(INT0_vect){											//Reset INT
-	TCNT1	=	0;
+	TCNT1=0;										//Reset counter
 	pin[5]=0;
 	pin[4]=0;
 	pin[3]=0;
@@ -76,13 +75,13 @@ ISR(INT0_vect){											//Reset INT
 }
 
 ISR(INT1_vect){											//Pause INT
-	TCCR1B	&=(~(1<<CS12));
-	TCCR1B	&=(~(1<<CS11));
-	TCCR1B	&=(~(1<<CS10));
+	TCCR1B	&=(~(1<<CS12));									//Turn off clock
+	TCCR1B	&=(~(1<<CS11));									//Turn off clock
+	TCCR1B	&=(~(1<<CS10));									//Turn off clock
 }
 
 ISR(INT2_vect){											//Resume INT
-	TCCR1B	|=	(1<<WGM12)	|	(1<<CS12)	|	(1<<CS10);
+	TCCR1B	|=	(1<<WGM12)	|	(1<<CS12)	|	(1<<CS10);		//Turn on clock again with same prescaller
 }
 
 
@@ -100,7 +99,7 @@ int main(){
 	while(1){
 		for(i=0;i<6;i++){
 			PORTA	=	(1<<i);					//7 segments always on
-			PORTC	=	pin[i];					//print
+			PORTC	=	pin[i];					//print hours, mins and seconds
 			_delay_ms(3);
 		}
 	}
